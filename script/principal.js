72237a7db85e0ebb43dac8ba9a21ec85402dc88e
ARSENAL DEBCT <108934637+lucas0520@users.noreply.github.com>
2023-10-02 11:36:58 -0300

    Add files via upload

--- script/principal.js
+++ script/principal.js
@@ -1,32 +1,56 @@
-escrever = (msg) => alert (msg);
-soma = (a,b) => a + b;
+escrever = (msg) => alert(msg);
+soma = (a,b) => Number(a) + Number(b);
 sub = (a,b) => a - b;
 div = (a,b) => a / b;
 mult = (a,b) => a * b;
-raiz  = a => Math.sqrt(a);
+raiz = a => Math.sqrt(a);
 eq2grau = (a,b,c) => {
-    if(a == 0) return "não é uma equação do segundo gral"
+    if(a == 0) return "Não é uma equação do Segundo Grau.";
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
-    if(delta < 0) return "não possui raiz real.";
-    if(delta == 0) return "x1 = x2" + div (-b,mult(2,a));
-    return "x1 " + div(soma(-b,raiz(delta))),mult(2,a) +
-           "x2 " + div(sub(-b,raiz(delta))),mult(2,a);
-
+    if(delta < 0) return "Não Possui raiz real.";
+    if(delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           "x2 " + div(sub(-b,raiz(delta)),mult(2,a));
 }
 let a = "";
 let b = "";
-let op= "";
+let op = "";
 let valor = "";
 let tem_ponto = false;
+let desligada = false;
+function zerar(){
+    if(desligada) return;
+    a = "";
+    b = "";
+    op = "";
+    valor = "";
+    tem_ponto = false;
+    mostra_resultado(0);
+}
+function desligar(){
+    if(desligada){
+        desligada = false;
+        zerar();
+    }else{
+        zerar();
+        mostra_resultado("");
+        desligada = true;
+    }
+    return desligada;
+}
 function mostra_resultado(resul){
-    document.getElementById("resultado").value = resul;
+    if(desligada) return;
+     document.getElementById("resultado").value = resul;
 }
 function operacao(nova_op){
+    if(desligada) return;
     op = nova_op;
     a = valor;
     valor = "";
+    tem_ponto = false;
 }
 function calcula(){
+    if(desligada) return;
     if(op != ""){
         b = valor;
         valor = "";
@@ -37,16 +61,18 @@ function calcula(){
         a = "";
         b = "";
         tem_ponto = false;
+
     }
 }
 function digitando(tecla){
+    if(desligada) return;
     if(tecla == "."){
-        if(!tem_ponto){
+         if(!tem_ponto){
             valor = valor + tecla;
             mostra_resultado(valor);
             tem_ponto = true;
-        }
-        return;
+         }
+         return;
     }
     valor = valor + tecla;
     mostra_resultado(valor);
